7d3764752f698a9f70bdb25bd4d1ce8c5b3987a8
Elin <elin.simonsson@outlook.com>
2023-01-02 10:58:12 +0100

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Elin Simonsson on 2023-01-02.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
